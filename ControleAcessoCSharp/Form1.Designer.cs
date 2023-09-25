e2d81ca5812c43509f39622c02b44d0dd8294c98
Verônica Helena <desenvolveronica@gmail.com>
2023-09-25 09:00:11 -0300

    Ajustes Aplicação

--- ControleAcessoCSharp/Form1.Designer.cs
+++ ControleAcessoCSharp/Form1.Designer.cs
@@ -45,10 +45,8 @@
             this.invalidarAtivarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.acessoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ativarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -60,7 +58,7 @@
             this.sairToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(888, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(482, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -153,6 +151,7 @@
             // controleDeAcessoToolStripMenuItem
             // 
             this.controleDeAcessoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ativarToolStripMenuItem,
             this.invalidarAtivarToolStripMenuItem,
             this.acessoToolStripMenuItem1});
             this.controleDeAcessoToolStripMenuItem.Name = "controleDeAcessoToolStripMenuItem";
@@ -162,13 +161,13 @@
             // invalidarAtivarToolStripMenuItem
             // 
             this.invalidarAtivarToolStripMenuItem.Name = "invalidarAtivarToolStripMenuItem";
-            this.invalidarAtivarToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
-            this.invalidarAtivarToolStripMenuItem.Text = "Invalidar - Ativar";
+            this.invalidarAtivarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.invalidarAtivarToolStripMenuItem.Text = "Invalidar";
             // 
             // acessoToolStripMenuItem1
             // 
             this.acessoToolStripMenuItem1.Name = "acessoToolStripMenuItem1";
-            this.acessoToolStripMenuItem1.Size = new System.Drawing.Size(161, 22);
+            this.acessoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.acessoToolStripMenuItem1.Text = "Acesso";
             // 
             // sairToolStripMenuItem
@@ -177,41 +176,24 @@
             this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.sairToolStripMenuItem.Text = "Sair";
             // 
-            // button1
+            // ativarToolStripMenuItem
             // 
-            this.button1.Location = new System.Drawing.Point(796, 338);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 44);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(859, 265);
-            this.dataGridView1.TabIndex = 2;
+            this.ativarToolStripMenuItem.Name = "ativarToolStripMenuItem";
+            this.ativarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ativarToolStripMenuItem.Text = "Ativar";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(482, 293);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Controle de Acesso";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -236,8 +218,7 @@
         private System.Windows.Forms.ToolStripMenuItem controleDeAcessoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem invalidarAtivarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem acessoToolStripMenuItem1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem ativarToolStripMenuItem;
     }
 }
 
